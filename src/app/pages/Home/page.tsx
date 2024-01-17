1c93115684ba26713bc820e4aa371ce029744c71
naaoufal <naoufelbenmensour@gmail.com>
2024-01-17 15:46:29 +0100

    add some edits

--- src/app/pages/Home/page.tsx
+++ src/app/pages/Home/page.tsx
@@ -184,6 +184,7 @@ export default function Home() {
                              color="primary"
                             onClick={handleOpen}
                         >Add new task</Button>
+                        <text>test test</text>
                     </Grid>
                     <Button onClick={() => router.push("/finishedTasks")}>Finished Tasks</Button>
                 </div>
