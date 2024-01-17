7562b521a9be34cf90aa11d4ca0f052f1780e3d3
naaoufal <naoufelbenmensour@gmail.com>
2024-01-17 15:35:13 +0100

    add some edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,15 @@ import Image from 'next/image';
 import HomeScreem from './pages/Home/page';
 import { Button } from '@mui/material';
 import { useRouter } from 'next/navigation';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize('G-0B0Y3289GS');
+
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: window.location.pathname, 
+  title: "Custom Title" 
+});
 
 export default async function Home() {
 
