329dfe4059615df44a9a885a4bfaedd8aa2c9fa7
naaoufal <naoufelbenmensour@gmail.com>
2024-01-18 15:30:04 +0100

    change react-ga library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import HomeScreen from './pages/Home/page';
 import { Button } from '@mui/material';
 import { useRouter } from 'next/navigation';
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 import { useEffect } from 'react';
 
 ReactGA.initialize('G-3EY3XB1VXH');
@@ -11,7 +11,12 @@ ReactGA.initialize('G-3EY3XB1VXH');
 export default function Home() {
 
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    // ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({ 
+      hitType: "pageview", 
+      page: window.location.pathname + window.location.search, 
+      title: "Custom Title" 
+    });
   });
 
   return (
